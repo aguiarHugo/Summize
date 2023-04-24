71684d634539cd7766be7db68e5268536ce8183b
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-24 11:27:01 -0300

    Debug

--- src/services/article.js
+++ src/services/article.js
@@ -9,7 +9,7 @@ export const articleApi = createApi({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', rapidApiKey);
-      headers.set('X-RapidAPI-Host', 'application/octet-stream');
+      headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
       return headers
     }
